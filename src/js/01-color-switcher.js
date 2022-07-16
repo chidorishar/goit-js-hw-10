e5e683967f17480bd7557882c4fbbf91d4539edd
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-07-16 19:48:00 +0300

    Solved first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,40 @@
+const TIMER_INTERVAL = 1000;
+const cntrlBttns = {
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+};
+let isChangingActive = false;
+let timerID = null;
 
+//main function
+(() => {
+  Object.values(cntrlBttns).forEach(bttn =>
+    bttn.addEventListener('click', onControlButtonClick)
+  );
+  toggleButtonEnabledState(cntrlBttns.stop);
+})();
+
+function onControlButtonClick() {
+  if (isChangingActive) {
+    clearInterval(timerID);
+  } else {
+    setRndBackgroundColor();
+    timerID = setInterval(setRndBackgroundColor, TIMER_INTERVAL);
+  }
+  Object.values(cntrlBttns).forEach(bttn => toggleButtonEnabledState(bttn));
+  isChangingActive = !isChangingActive;
+}
+
+function setRndBackgroundColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function toggleButtonEnabledState(bttn) {
+  bttn.hasAttribute('disabled')
+    ? bttn.removeAttribute('disabled')
+    : bttn.setAttribute('disabled', '');
+}
