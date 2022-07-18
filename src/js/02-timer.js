246ac5369176834bb03423fc2018a39838638897
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-07-18 21:00:06 +0300

    Added timer class

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,8 +3,42 @@ import 'flatpickr/dist/flatpickr.min.css';
 import { toggleButtonEnabledState, setButtonEnabledState } from './common';
 
 const elStartButton = document.querySelector('[data-start]');
+let timer = null;
 let selectedTime = 0;
 
+class Timer {
+  #deltaTime;
+  #endTime;
+  #isActive;
+
+  constructor(onTick = () => {}) {
+    this.onTick = onTick;
+    this.#deltaTime = 0;
+    this.#endTime = -1;
+    this.#isActive = false;
+  }
+
+  #tick() {
+    let { deltaTime, endTime } = this;
+    this.#deltaTime = this.#endTime - Date.now();
+    this.onTick(this.#deltaTime);
+    console.log(this.#deltaTime);
+  }
+
+  setTime(time) {
+    this.#endTime = time;
+  }
+
+  start() {
+    if (this.#isActive || this.#endTime < 0) {
+      console.log('Timer start dropped');
+      return;
+    }
+
+    setInterval(this.#tick.bind(this), 1000);
+    this.#isActive = true;
+  }
+}
 (() => {
   flatpickr('#datetime-picker', {
     enableTime: true,
@@ -13,6 +47,7 @@ let selectedTime = 0;
     minuteIncrement: 1,
     onClose: onDataSelected,
   });
+  timer = new Timer(onTimerTick);
   elStartButton.addEventListener('click', onStartClick);
   toggleButtonEnabledState(elStartButton);
 })();
@@ -33,8 +68,11 @@ function onStartClick() {
     alert('Please choose a date in the future');
     return;
   }
+  timer.setTime(selectedTime);
+  timer.start();
 }
 
+function onTimerTick({ days, hours, minutes, seconds }) {}
 function isTimeInFuture(time) {
   return time > Date.now();
 }
