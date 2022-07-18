cfbb2ad6d369173e9d5f484f8e3fac22cea23b4f
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-07-18 20:07:51 +0300

    Added config for data lib

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,5 +3,12 @@ import 'flatpickr/dist/flatpickr.min.css';
 import toggleButtonEnabledState from './common';
 
 (() => {
-  flatpickr('#datetime-picker', { enableTime: true });
+  flatpickr('#datetime-picker', {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose: onDataSelected,
+  });
+  toggleButtonEnabledState(elStartButton);
 })();
