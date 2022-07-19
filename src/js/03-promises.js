632afe8c096720629172dd67fbb038cc37ec6e7e
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-07-19 21:03:13 +0300

    Solved task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,49 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formEl = document.querySelector('.form');
+const promisesOptions = {
+  delay: 0,
+  step: 0,
+  amount: 0,
+};
+
+//main function
+(() => {
+  formEl.addEventListener('submit', onFormSubmit);
+})();
+
+function onFormSubmit(event) {
+  readPromisesCreationOptions(promisesOptions);
+  const { delay, step, amount } = promisesOptions;
+
+  for (let i = 0; i < amount; i++) {
+    createPromise(i + 1, delay + step * i)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+  event.preventDefault();
+}
+
+function readPromisesCreationOptions(opt) {
+  Object.keys(opt).forEach(key => {
+    opt[key] = parseInt(formEl.elements[key].value);
+  });
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) =>
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay)
+  );
 }
