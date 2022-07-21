4e8d72f9b32aa425145d0c33bd001e5ecc4c0d83
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-07-22 00:48:11 +0300

    Added handlebars helper for number thousand separator

--- src/js/markup/helpers/handlebarsHelpers.js
+++ src/js/markup/helpers/handlebarsHelpers.js
@@ -3,3 +3,7 @@ const Handlebars = require('handlebars');
 Handlebars.registerHelper('isOverZero', function (value) {
   return value > 0;
 });
+
+Handlebars.registerHelper('numberSeparatedWithWhitespace', function (value) {
+  return value.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ' ');
+});
