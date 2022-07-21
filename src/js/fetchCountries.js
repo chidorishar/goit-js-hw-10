c2c76b38063d3a5a03f5141119786b05cc02be9d
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-07-22 00:24:21 +0300

    Added countries fetch function

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,1 +1,11 @@
-export function fetchCountries(name) {}
+const URL = 'https://restcountries.com/v3.1/name/';
+const FILTERS = '?fields=name,capital,population,flags,languages';
+
+export function fetchCountries(name) {
+  return fetch(URL + name + FILTERS).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
